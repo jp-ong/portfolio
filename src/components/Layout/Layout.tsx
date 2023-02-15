917d2fd3c514dc39ee5825c6d53d6b1db5e23fab
John Paul Ong <jpong5202@gmail.com>
2023-02-15 14:01:38 +0800

    Initial Setup

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>John Paul Ong | Web Developer</title>
+        <meta
+          name="description"
+          content="I'm a fresh graduate web developer based in Metro Manila, Philippines. I enjoy the challenge in taking on project requirements and complex designs and implementing them into simple and beautiful systems for clients and customers while doing it in the most elegant and efficient way I am capable of for other developers."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
