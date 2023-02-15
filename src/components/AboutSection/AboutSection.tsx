b0a44addc4fb8e2ebf4e5729a20c444ee6c2c7b2
John Paul Ong <jpong5202@gmail.com>
2023-02-15 22:57:25 +0800

    Update About Me

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -3,11 +3,12 @@ const AboutSection = () => {
     <section>
       <h1>About Me</h1>
       <p>
-        I'm a fresh graduate web developer based in Metro Manila, Philippines. I
-        enjoy the challenge in taking on project requirements and complex
-        designs and implementing them into simple and beautiful systems for
-        clients and customers while doing it in the most elegant and efficient
-        way I am capable of for other developers.
+        I'm a fresh graduate in Information Technology based in Metro Manila,
+        Philippines. I specialize in Web Development and enjoy the challenge in
+        taking on project requirements and complex designs and implementing them
+        into simple and beautiful systems for clients and customers while doing
+        it in the most elegant and efficient way I am capable of for other
+        developers.
       </p>
       <p>
         I am most confident in my ability to work with JavaScript and its
