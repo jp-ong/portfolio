5b874857ab3e1014d2d09b2115be9b102394b687
John Paul Ong <jpong5202@gmail.com>
2023-02-17 01:01:07 +0800

    Add desktop view css

--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -9,7 +9,7 @@ import { LinkedInIcon } from "../icons/LinkedInIcon";
 
 const InfoSection = () => {
   return (
-    <section id="info">
+    <section className={styles.section} id="info">
       <div className={styles.panel}>
         <h1 className={styles.greeting}>
           <small>Hello, I'm</small>
